eb3a35ad4fd1a69b35a20684783e892cd7bbb63f
Luke-lab2002 <nguyenvuminhcuong2002@gmail.com>
2024-12-14 19:17:03 +0700

    commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Mistral Chemistry AI
+
+## Introduction
+
+### Login:
+![img.png](images/img.png)
+
+### Chat
+![img.png](images/img01.png)
+
+## Setup
+
+```bash 
+git clone https://github.com/Luke-lab2002/StreamlitChemistryChatBot.git
+
+cd StreamlitChemistryChatBot
+
+streamlit run app.py
+```
--- web/login.py
+++ web/login.py
@@ -10,26 +10,40 @@ class Login:
         self.login_url = login_url
         self.controller = cookie_controller
 
+
+    def login(self, email, password):
+        login_inf = requests.post(self.login_url, data=form_request(email, password)).json()
+        if login_inf["state"] == "success":
+            self.controller['access_token'] = login_inf["access_token"]
+            self.controller['user_id'] = login_inf["id"]
+            self.controller.save()
+
+            st.success("Login Successful")
+            st.rerun()
+        else:
+            st.error("Login Failed")
+
+    @st.dialog("register")
+    def register(self):
+        st.write("Register")
+        if st.button("submit"):
+            st.rerun()
+
+
     def display(self):
 
-        st.title("Đăng ký tài khoản")
+        _,center,_ = st.columns(3)
+        center.header("H2SO4.ai")
+        center.image("icons/flask.png", width=100)
 
-        with st.form("my_form"):
-            email = st.text_input("Tên đăng nhập")
-            password = st.text_input("Mật khẩu", type="password")
-            submitted = st.form_submit_button("Đăng nhập")
-            if submitted:
-                # Kiểm tra thông tin đăng nhập (thay thế bằng logic xác thực của bạn)
-                login_inf = requests.post(self.login_url, data=form_request(email, password)).json()
-                if login_inf["state"] == "success":
-                    # st.session_state['authenticated'] =
-                    # Thiết lập thời gian hết hạn cho cookie 'access_token'
-                    self.controller['access_token'] = login_inf["access_token"]
-                    self.controller['user_id'] = login_inf["id"]
-                    self.controller.save()
-                    # st.session_state["userid"] = login_inf["id"]
-                    st.success("Đăng nhập thành công!")
-                    st.rerun()
-                else:
-                    st.error("Tên đăng nhập hoặc mật khẩu không đúng.")
+        st.title("Login")
+        email = st.text_input("Email")
+        password = st.text_input("Password", type="password")
 
+        left, mid = st.columns(2)
+        if left.button("Login", use_container_width=True):
+            self.login(email, password)
+
+
+        if mid.button("Register", use_container_width=True):
+            self.register()
